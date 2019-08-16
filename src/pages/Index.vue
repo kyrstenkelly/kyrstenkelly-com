c20f69a4c6119bd02ed8600aed746533489ac950
Kyrsten Kelly <kyrsten@udacity.com>
2019-08-15 21:51:57 -0500

    Updates to the homepage layout.

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,10 +1,11 @@
 <template>
-  <Layout class="home">
-    <div class="intro">
-      <g-image :src="image" class="profile-image"/>
-      <h1 class="title">Hi, I'm Kyrsten.</h1>
+  <Layout>
+    <div class="home">
+      <div class="intro">
+        <g-image :src="image" class="profile-image"/>
+        <h1 class="title">Hi, I'm Kyrsten.</h1>
+      </div>
     </div>
-
   </Layout>
 </template>
 
@@ -22,53 +23,60 @@ export default {
 </script>
 
 <style lang="scss">
-.intro {
+.home {
+  flex: 1;
   display: flex;
   justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  margin: spacing(5) spacing(2);
 
-  .profile-image {
-    width: 60%;
-    border-radius: 50%;
-    margin-right: spacing(0);
-    transition: width .2s ease;
-  }
-
-  .title {
-    font-size: rem(36);
-    transition: font-size .2s ease;
-  }
-
-  @include media($screen-sm) {
-    margin: spacing(7) spacing(2);
-    flex-direction: row;
-
+  .intro {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    margin-top: 0;
+  
     .profile-image {
-      width: 25%;
-      max-width: 150px !important;
-      margin-right: spacing(3);
+      width: 60%;
+      border-radius: 50%;
+      margin-right: spacing(0);
+      transition: width .2s ease;
     }
-
+  
     .title {
-      font-size: rem(44);
+      font-size: rem(36);
+      transition: font-size .2s ease;
     }
-  }
-
-  @include media($screen-md) {
-    .title {
-      font-size: rem(56);
+  
+    @include media($screen-sm) {
+      // Subtract ~ the height of the header so the intro looks centered
+      margin-top: rem(-100);
+      flex-direction: row;
+  
+      .profile-image {
+        width: 25%;
+        max-width: 150px !important;
+        margin-right: spacing(3);
+      }
+  
+      .title {
+        font-size: rem(44);
+      }
     }
-  }
-
-  @include media($screen-lg) {
-    .profile-image {
-      margin-right: spacing(5);
+  
+    @include media($screen-md) {
+      .title {
+        font-size: rem(56);
+      }
     }
-
-    .title {
-      font-size: rem(64);
+  
+    @include media($screen-lg) {
+      .profile-image {
+        margin-right: spacing(5);
+      }
+  
+      .title {
+        font-size: rem(64);
+      }
     }
   }
 }
