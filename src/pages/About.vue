4db5c71e4241947552a8883e447393ac6bda60a6
Kyrsten Kelly <kyrsten@udacity.com>
2019-08-16 16:15:28 -0500

    Update About Me page...a little.

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,14 +1,38 @@
 <template>
-  <Layout>
-    <h1>About us</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+  <Layout class="about">
+    <h1 class="title">About Me</h1>
+    <ul class="links">
+      <li class="links__item">
+        <a class="link" href="https://www.linkedin.com/in/kyrsten-kelly/" target="_blank">LinkedIn</a>
+      </li>
+      <li class="links__item">
+        <a class="link" href="https://github.com/kyrstenkelly" target="_blank">Github</a>
+      </li>
+    </ul>
+    <p>
+      I'm a full-stack Software Engineer in Austin, TX. I have a passion for problem solving, and love working alongside collaborative people.
+    </p>
   </Layout>
 </template>
 
 <script>
 export default {
   metaInfo: {
-    title: 'About us'
+    title: 'About Me'
   }
 }
 </script>
+
+<style lang="scss">
+.about {
+  .links {
+    display: flex;
+    list-style: none;
+    padding: 0;
+    
+    &__item {
+      margin-right: spacing(2);
+    }
+  }
+}
+</style>
